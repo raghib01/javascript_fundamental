391cf8ae799ded13f199357b5f8fbc4dc7682638
raghib01 <rakibulislamragib2@gmail.com>
2023-03-01 01:04:00 +0600

    commit message

---
+++ iterator_genaretor/async_It&gen.js
@@ -0,0 +1,30 @@
+const axios = require("axios").default;
+
+async function getUsers() {
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const { data: users } = await axios.get(url);
+  return users;
+}
+
+async function* getUsersPost(users) {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  for (let i = 0; i < users.length; i++) {
+    const { data: posts } = await axios.get(`${url}?useId=${users[i].id}`);
+    yield posts;
+  }
+}
+
+getUsers()
+  .then(async (users) => {
+    const userIterator = await getUsersPost(users);
+    // await userIterator.next();
+    // await userIterator.next();
+    // console.log((await userIterator.next()).value);
+
+    for await (let i of getUsersPost(users)) {
+      console.log(i.map((data) => data.title));
+    }
+  })
+  .catch((e) => {
+    console.log(e);
+  });
